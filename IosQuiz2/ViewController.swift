6bfb169ca516bd826e3473d3ace6c56e38f2d4a3
Kaleb Spillman <kalebspillman@Interapts-MacBook-Pro-5.local>
2018-10-25 12:27:45 -0500

    first commit

--- IosQuiz2/ViewController.swift
+++ IosQuiz2/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        guard
+        let firstName = firstNameTextField.text, !firstName.isEmpty,
+        let lastName = lastNameTextField.text, !lastName.isEmpty,
+        let email = emailTextField.text, !email.isEmpty
+            else {
+             return
+        }
+        let person = UIAlertController(title: "Greetings", message: "hello \(firstName) \(lastName) your email address is \(email).", preferredStyle: UIAlertController.Style.alert)
+        let dismissAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) {UIAlertAction in}
+        self.present(person, animated: true, completion: nil)
+    }
+
+
 
 
 }
-
---
+++ IosQuiz2/personClass.swift
@@ -0,0 +1,23 @@
+//
+//  PersonClass.swift
+//  IosQuiz2
+//
+//  Created by Kaleb Spillman on 10/25/18.
+//  Copyright © 2018 Kaleb Spillman. All rights reserved.
+//
+
+import Foundation
+
+class PersonClass {
+    var firstName: String
+    var lastName: String
+    var email: String
+
+    init(firstName: String, lastName: String, email: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        
+    }
+    
+}
